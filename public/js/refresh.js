997d47c852a3ef906f61ccf84ba032e216bb9aa3
Gregory Vicent <gregory.work14@gmail.com>
2021-04-04 16:49:10 -0400

    change in js

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -1,7 +1,12 @@
 export default function refresh() {
   const $buttonRefresh = document.getElementById("button-refresh");
 
-  $buttonRefresh.addEventListener("click", () => {
-    window.location.href = "/";
+  document.addEventListener("click", (e) => {
+    if(e.target === $buttonRefresh) { 
+      e.preventDefault();
+      
+      let search = window.location.search;
+      window.location.href = `/${search}`;
+    }
   })
 }
